03a4888a087fc4fae853c3143aa969b93bf67666
Dylanzo-55 <dylanthomasmathews55@gmail.com>
2026-09-03 19:20:35 +0200

    made some changes

--- Boolean_expressions/Program.cs
+++ Boolean_expressions/Program.cs
@@ -11,3 +11,22 @@ Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 dig
 Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
 Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
 
+/* 
+int value = (int)1.5m; // casting truncates
+Console.WriteLine(value);
+
+*/
+
+int value2 = Convert.ToInt32(1.5m); // converting rounds up
+Console.WriteLine(value2);
+
+string value = "102";
+int result = 0;
+if (int.TryParse(value, out result))
+{
+   Console.WriteLine($"Measurement: {result}");
+}
+else
+{
+   Console.WriteLine("Unable to report the measurement.");
+}
